fdf191adda2d43395cbfa1e72c8a38281fa7438b
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-05-29 20:10:49 +0530

    Practice set 2

---
+++ ch_2_veriable_datatype/ch_2_PracticeSet.py
@@ -0,0 +1,16 @@
+# Q.1 Write a pythin program to sum two numbers
+
+a = 3
+b = 5
+
+sum = (a + b)
+print("Q.1 Sum is ->",sum)
+
+# Q.2 Write a python program to print remender when divided by Z
+
+c = 50
+d = 5
+rem = c % d
+print("Q.2 Remender is ->",rem)
+
+# Q.3
