c2eaca92d249ab316c70c3514c312a5fa509c69a
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-05-30 07:38:11 +0530

    Practice Set 2

--- ch_2_veriable_datatype/ch_2_PracticeSet.py
+++ ch_2_veriable_datatype/ch_2_PracticeSet.py
@@ -4,13 +4,34 @@ a = 3
 b = 5
 
 sum = (a + b)
-print("Q.1 Sum is ->",sum)
+print("Q.1-> Sum is ->",sum)
 
 # Q.2 Write a python program to print remender when divided by Z
 
 c = 50
 d = 5
 rem = c % d
-print("Q.2 Remender is ->",rem)
+print("Q.2-> Remender is ->",rem)
 
-# Q.3
+# Q.3 check the type of variable assineed using in input() function
+
+a = input("Input the value :-")
+print("Q.3-> ",type(a))
+
+
+# Q.4 Use comparison oprator to find out whether a given number is greater than 'b' or not 
+# Take e = 24 and f = 80
+e = int(input("Enter 1st number :-")) 
+f = int(input("Enter 2nd number:-"))
+print("Q.4-> a is greater than b :-",e>f)
+
+# Q.5 Write a python program to fing the avreage of given two number by the user
+g = int(input("Enter 1st number to find the avreage :-")) 
+h = int(input("Enter 2nd number to find the avreage :-"))
+avr = (g + h)/2 
+print("Q.5-> Avrage of given two number is ",avr)
+
+# Q.6 Write a pythin program to find the square of given number by the user
+
+squ = int(input("Enter the Number for the Square:-"))
+print("Q.6-> The Square is:-",squ*squ)
---
+++ ch_2_veriable_datatype/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+a = 3
+# b = 5
+
+# sum = (a + b)
+# print("Q.1 Sum is ->",sum)
