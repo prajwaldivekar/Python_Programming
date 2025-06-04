65ed679262dd4fedb512c33494b3608692dfeccd
prajwaldivekar <prajwaldivekar2007@gmail.com>
2025-06-04 08:04:27 +0530

    String Slicing

---
+++ ch_3_string/string_slicing.py
@@ -0,0 +1,19 @@
+# A string in python can be sliced for getting for a part of string
+
+name = 'Prajwal'
+
+nameshort = name[0:4]
+print(nameshort)
+
+chareacter = name[0]
+print(chareacter)
+
+# print(name[1:4])  #this is smilar to below {hint : change the curessponding digits sign}
+# print(name[-4:-1]) #[1:4] == [-4:-1]
+
+a = '012345678'
+print(a[1:7:2]) 
+#Output comes--> Started form 1 index goes upto 7 
+# take a gap of 2
+#[start : end : gap] ==[1:7:2]
+
